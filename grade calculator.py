be2a3d3c569212845692b8a2b6127c91c6f74b38
Smriti Jaiswal <smritix@gmail.com>
2026-04-23 11:07:17 +0100

    test code

---
+++ grade calculator.py
@@ -0,0 +1,10 @@
+a= int(input("Enter a number"))
+if a%3==0 and a%5==0:
+    print ("FizzBuzz")
+elif 12 a%3==0:
+    print ("Fizz")
+elif a%5==0:
+    print ("Buzz")
+
+elif a%3!=0 or a%5!=0:
+    print ("Hello World")
